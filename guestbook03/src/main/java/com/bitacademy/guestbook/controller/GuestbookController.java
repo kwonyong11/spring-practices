49d681da462127c7897a1678c0c263b869d14ec6
BIT_R <BIT_R@DESKTOP-CTRFTLR>
2020-12-31 14:16:25 +0900

    spring guestbook03 추가

---
+++ guestbook03/src/main/java/com/bitacademy/guestbook/controller/GuestbookController.java
@@ -0,0 +1,47 @@
+package com.bitacademy.guestbook.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bitacademy.guestbook.repository.GuestbookRepository;
+import com.bitacademy.guestbook.vo.GuestbookVo;
+
+@Controller
+public class GuestbookController {
+	
+	@Autowired
+	private GuestbookRepository guestbookRepository;
+	
+	@RequestMapping("")
+	public String index(Model model) {
+		List<GuestbookVo> list = guestbookRepository.findAll();
+		model.addAttribute("list", list);
+		
+		return "index";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(GuestbookVo vo) {
+		guestbookRepository.insert(vo);
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/delete/{no}", method=RequestMethod.GET)
+	public String delete(@PathVariable(value="no") Long no, Model model) {
+		model.addAttribute("no", no);
+		return "deleteform";
+	}
+	
+	@RequestMapping(value="/delete/{no}", method=RequestMethod.POST)
+	public String delete(@PathVariable(value="no") Long no, String password) {
+		guestbookRepository.delete(no, password);
+		return "redirect:/";
+	}
+}
